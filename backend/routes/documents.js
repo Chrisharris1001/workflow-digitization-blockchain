ff34aecbb8cb020c284b8cf9c3af8bbe42785f39
Cristea Razvan <81560252+Chrisharris1001@users.noreply.github.com>
2025-05-14 17:48:13 +0300

    initialize project structure with Next.js, add configuration files

--- backend/routes/documents.js
+++ backend/routes/documents.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const { InfuraProvider, Wallet, Contract, keccak256 } = require('ethers');
 require('dotenv').config();
 
